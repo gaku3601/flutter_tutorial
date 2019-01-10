5d61e2433c5afcca6cc1b143aacf463577b8dbf7
gaku3601 <pro.gaku@gmail.com>
2019-01-10 09:28:52 +0900

    listviewに変更

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
           Image.asset(
             'images/lake.jpg',
